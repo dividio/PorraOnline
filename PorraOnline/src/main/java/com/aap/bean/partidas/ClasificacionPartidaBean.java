d9e1914cb7be05282bc030829233ff19659fe2e7
David Ballesteros <dividio@gmail.com>
2013-03-07 21:30:12 +0100

    - Añadida clasficación muy básica. La lista de usuarios registrados en la partida ordenados alfabéticamente.

--- PorraOnline/src/main/java/com/aap/bean/partidas/ClasificacionPartidaBean.java
+++ PorraOnline/src/main/java/com/aap/bean/partidas/ClasificacionPartidaBean.java
@@ -1,10 +1,13 @@
 package com.aap.bean.partidas;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 import com.aap.dto.Partidas;
@@ -20,6 +23,8 @@ public class ClasificacionPartidaBean implements Serializable {
     
     private Partidas partida = new Partidas();
     
+    private List<Object[]> clasificacion = new ArrayList<Object[]>();
+    
     public Long getIdPartida() {
 		return idPartida;
 	}
@@ -29,7 +34,20 @@ public class ClasificacionPartidaBean implements Serializable {
 		if(idPartida != null) {
 			Session session = Contexts.getHibernateSession();
 			partida = (Partidas) session.get(Partidas.class, idPartida);
+			cargarClasificacion();
 		}
+	}
+	
+	private void cargarClasificacion() {
+		Session session = Contexts.getHibernateSession();
+		String hql = "select USU.usu_username " +
+						"from Partidas PA " +
+						"join PA.usuarios USU " +
+						"where PA.pa_id = :ID_PARTIDA " +
+						"order by USU.usu_username";
+		Query hqlQ = session.createQuery(hql);
+		hqlQ.setLong("ID_PARTIDA", partida.getPa_id());
+		clasificacion = hqlQ.list();
 	}
 
 	public Partidas getPartida() {
@@ -38,6 +56,14 @@ public class ClasificacionPartidaBean implements Serializable {
 
 	public void setPartida(Partidas partida) {
 		this.partida = partida;
+	}
+
+	public List<Object[]> getClasificacion() {
+		return clasificacion;
+	}
+
+	public void setClasificacion(List<Object[]> clasificacion) {
+		this.clasificacion = clasificacion;
 	}
 	
 	

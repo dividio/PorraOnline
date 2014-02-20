2a7b690845233b63bc307efaf60580f67f7a27cf
David Ballesteros <dividio@gmail.com>
2014-02-20 18:24:16 +0100

    - Correcciones en el sistema de login

--- PorraOnline/src/main/webapp/js/directives/loginDirective.js
+++ PorraOnline/src/main/webapp/js/directives/loginDirective.js
@@ -20,47 +20,37 @@ directives.directive('login', ['Login', 'User', 'Alertas', function(Login, User,
         	return $scope.loginCtrl = this;
         }],
         link: function(scope, iElement, iAttrs) {
-        	navigator.id.watch({
-        		loggedInUser: User.getUser().usu_email,
-        		onlogin: function(assertion) {
-        		    // A user has logged in! Here you need to:
-        		    // 1. Send the assertion to your backend for verification and to create a session.
-        		    // 2. Update your UI.
-//        		    $.ajax({ /* <-- This example uses jQuery, but you can use whatever you'd like */
-//        		      type: 'POST',
-//        		      url: '/auth/login', // This is a URL on your website.
-//        		      data: {assertion: assertion},
-//        		      success: function(res, status, xhr) { window.location.reload(); },
-//        		      error: function(res, status, xhr) { alert("login failure" + res); }
-//        		    });
-        			  if(!User.getUser().usu_email) {
-        				  Login.login(assertion).then(
-        					function(value) {
-        						User.setUser(value.usuario);
-        						scope.currentUser = value.usuario;
-        					},
-        					Alertas.mostrarMensajes);
-        			  }
-        		},
-        		onlogout: function() {
-        		    // A user has logged out! Here you need to:
-        		    // Tear down the user's session by redirecting the user or making a call to your backend.
-//        		    $.ajax({
-//        		      type: 'POST',
-//        		      url: '/auth/logout', // This is a URL on your website.
-//        		      success: function(res, status, xhr) { window.location.reload(); },
-//        		      error: function(res, status, xhr) { alert("logout failure" + res); }
-//        		    });
-        		    
-        			  Login.logout().then(
-        				function(value) {
-        					User.setUser({});
-        					scope.currentUser = {};
-        					//window.location.reload();
-        				},
-        				Alertas.mostrarMensajes);
-        		}
-        	});
+        	if(navigator.id) {
+	        	navigator.id.watch({
+	        		loggedInUser: User.getUser().usu_email,
+	        		onlogin: function(assertion) {
+	        		    // A user has logged in! Here you need to:
+	        		    // 1. Send the assertion to your backend for verification and to create a session.
+	        		    // 2. Update your UI.
+	        			
+	        			if(!User.getUser().usu_email) {
+	        				Login.login(assertion).then(
+								function(value) {
+									User.setUser(value.usuario);
+									scope.currentUser = value.usuario;
+								},
+								Alertas.mostrarMensajes);
+	        			}
+	        		},
+	        		onlogout: function() {
+	        		    // A user has logged out! Here you need to:
+	        		    // Tear down the user's session by redirecting the user or making a call to your backend.
+	        		    
+						Login.logout().then(
+							function(value) {
+								User.setUser({});
+								scope.currentUser = {};
+								//window.location.reload();
+							},
+							Alertas.mostrarMensajes);
+	        		}
+	        	});
+	        }
         }
       };
   }]);

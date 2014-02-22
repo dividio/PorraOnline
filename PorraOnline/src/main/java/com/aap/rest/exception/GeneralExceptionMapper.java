2ed4758b0bb6bc0525d4bb961e89289b8f87ffcf
David Ballesteros <dividio@gmail.com>
2014-02-22 23:25:08 +0100

    - Ahora mostramos por el log la excepcion original cuando ocurre un error no esperado.

--- PorraOnline/src/main/java/com/aap/rest/exception/GeneralExceptionMapper.java
+++ PorraOnline/src/main/java/com/aap/rest/exception/GeneralExceptionMapper.java
@@ -7,16 +7,22 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.aap.rest.exception.ErrorResponseConverter.ErrorCode;
 
 @Provider
 public class GeneralExceptionMapper implements ExceptionMapper<Exception> {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(GeneralExceptionMapper.class);
+	
 	@Context
 	private HttpHeaders headers;
 
 	@Override
 	public Response toResponse(Exception e) {
+		log.error("Error no esperado", e);
 		return Response.status(Status.INTERNAL_SERVER_ERROR)
 		        .entity(new ErrorResponseConverter(e.getMessage(), e.getCause().getMessage(), ErrorCode.ERR0R))
 		        .type(headers.getMediaType()).build();
